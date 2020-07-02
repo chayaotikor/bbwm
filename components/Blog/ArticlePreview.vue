281db09f3d36d3269b995a1e087c2a8d16f59474
Chaya Otikor <c.otikor@gmail.com>
2020-07-02 12:53:41 -0600

    setup basic article and podcast components

---
+++ components/Blog/ArticlePreview.vue
@@ -0,0 +1,150 @@
+<template>
+     <div class="post-container">
+          <div class="line-point">
+            <i class="right-arrow" />
+            <span class='line'>
+            <h2 class="date">{{date}} </h2>
+            </span>
+            <span class="dot" />
+          </div>
+          <nuxt-link tag='article' :to="id" class="preview-container">
+              <div class="preview-thumbnail" :style="{backgroundImage: `url(${thumbnailUrl})`}"></div>
+              <div class="preview-content">
+                <h1 class="preview-title">{{title}}</h1>
+                <p class="preview-text">{{excerpt}}</p>
+              </div>
+          </nuxt-link>
+        </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        excerpt: {
+            type: String,
+            required: true
+        },
+        thumbnailUrl: {
+            type: String,
+            required: true
+        },
+        id: {
+            type: Number,
+            required: true
+        },
+        date: {
+            type: String,
+            required: true
+        },
+    }
+}
+</script>
+
+<style scoped>
+.post-container {
+  width: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: space-between;
+  align-items: space-between;
+  position: relative;
+}
+
+
+.line-point {
+  width: 40%;
+  height: 16px;
+  display: flex;
+  align-self: flex-end;
+  flex-flow: row-reverse nowrap;
+  align-items: center;
+  justify-content: flex-end;
+  position: absolute;
+  top: -8px;
+  left: -8px;
+}
+.line {
+  border: 1px solid white;
+  color: white;
+  background-color: white;
+  width: 100%;
+  height: 2px;
+  display: flex;
+  justify-content: center;
+}
+
+.date {
+  position: absolute;
+  top: -10px;
+  font-size: 16px;
+}
+
+.right-arrow {
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 3px;
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+}
+
+
+.dot {
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  border: 1px solid white;
+  background: white;
+  margin: 0;
+  z-index: 1;
+}
+.preview-container {
+  text-decoration: none;
+  width: 50%;
+  height: 45vh;
+  border: 1px solid white;
+  display: flex;
+  margin: 2.5% 5%;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  cursor: pointer;
+  align-self: flex-end;
+}
+
+
+.preview-thumbnail {
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  height: 75%;
+}
+
+.preview-content {
+  display: flex;
+  flex-flow: column nowrap;
+  padding: 2.5%;
+  width: 100%;
+  height:25%;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  background: white;
+  color: black;
+}
+
+.preview-title {
+  font-size: 1.8rem;
+}
+
+.preview-text {
+  font-size: 1.4rem;
+}
+
+</style>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,32 +1,31 @@
 <template>
   <div class="timeline-container">
     <section class="left-container">
-    
+      <PodcastPreview  v-for="post in posts" :key="post.id" :title="post.title"
+          :excerpt="post.excerpt"
+          :thumbnailUrl="post.thumbnailUrl"
+          :date="post.date"
+          :id="post.id"/>
     </section>
-    <span class="middle-line"></span>
+    <span class="middle-line" />
     <section class="right-container">
-        <div class="post-container" v-for="post in posts" :key="post.id">
-          <div class="line-point">
-            <i class="arrow right" />
-            <span class='line'>
-            <h2 class="date">{{post.date}} </h2>
-            </span>
-            <span class="dot" />
-          </div>
-          <nuxt-link tag='article' :to="post.id" class="preview-container right-side">
-              <div class="preview-thumbnail" :style="{backgroundImage: `url(${post.thumbnailUrl})`}"></div>
-              <div class="preview-content">
-                <h1 class="preview-title">{{post.title}}</h1>
-                <p class="preview-text">{{post.excerpt}}</p>
-              </div>
-          </nuxt-link>
-        </div>
+      <ArticlePreview  v-for="post in posts" :key="post.id" :title="post.title"
+          :excerpt="post.excerpt"
+          :thumbnailUrl="post.thumbnailUrl"
+          :date="post.date"
+          :id="post.id"/>
     </section>
   </div>
 </template>
 
 <script>
+import ArticlePreview from "@/components/Blog/ArticlePreview"
+import PodcastPreview from "@/components/Blog/PodcastPreview"
 export default {
+  components: {
+        ArticlePreview,
+        PodcastPreview
+    },
     data(){
         return{
             posts: [
@@ -50,6 +49,7 @@ export default {
 }
 </script>
 
+
 <style scoped>
 .timeline-container {
   display: flex;
@@ -58,9 +58,8 @@ export default {
   justify-content: space-evenly;
   align-items: flex-start;
   overflow-y: scroll;
-  border: 1px solid red;
   position: relative;
-  padding-top: 8rem;
+  padding: 8rem 0;
 }
 .middle-line {
   position: absolute;
@@ -87,121 +86,5 @@ export default {
   align-items: center;
 
 }
-.post-container {
-  width: 100%;
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: space-between;
-  align-items: space-between;
-  position: relative;
-}
 
-.line-point {
-  width: 40%;
-  height: 16px;
-  display: flex;
-  align-self: flex-end;
-  flex-flow: row-reverse nowrap;
-  align-items: center;
-  justify-content: flex-end;
-  position: absolute;
-  top: -8px;
-  left: -8px;
-}
-.line {
-  border: 1px solid white;
-  color: white;
-  background-color: white;
-  width: 100%;
-  height: 2px;
-  display: flex;
-  justify-content: center;
-}
-
-.date {
-  position: absolute;
-  top: -10px;
-  font-size: 16px;
-}
-
-.arrow {
-  border: solid white;
-  border-width: 0 3px 3px 0;
-  display: inline-block;
-  padding: 3px;
-
-}
-
-.left {
-  transform: rotate(135deg);
-  -webkit-transform: rotate(135deg);
-}
-.right {
-  transform: rotate(-45deg);
-  -webkit-transform: rotate(-45deg);
-}
-.dot {
-  height: 16px;
-  width: 16px;
-  border-radius: 50%;
-  border: 1px solid white;
-  background: white;
-  margin: 0;
-  z-index: 1;
-}
-.preview-container {
-  text-decoration: none;
-  width: 50%;
-  height: 50vh;
-  border: 1px solid white;
-  display: flex;
-  margin: 2.5% 5%;
-  flex-flow: column nowrap;
-  justify-content: center;
-  align-items: center;
-  border-radius: 10px;
-}
-
-.right-side {
-  align-self: flex-end;
-}
-
-.left-side {
-  align-self: flex-start;
-}
-
-.preview-thumbnail {
-  background-position: center;
-  background-size: cover;
-  width: 100%;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-  height: 75%;
-}
-
-.preview-content {
-  display: flex;
-  flex-flow: column nowrap;
-  padding: 2.5%;
-  width: 100%;
-  height:25%;
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
-  background: white;
-  color: black;
-}
-
-.preview-title {
-  font-size: 1.8rem;
-}
-
-.preview-text {
-  font-size: 1.4rem;
-}
-
-@media (min-width: 35rem) {
-  .post-preview {
-    width: 25rem;
-  }
-}
 </style>
