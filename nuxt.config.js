3a1d4792454c7249163b288dc6b4d788c3180f62
Chaya Otikor <c.otikor@gmail.com>
2020-07-14 14:27:05 -0600

    favicon added

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Dosis&family=Lato&family=Economica&display=swap"}
     ]
   },
